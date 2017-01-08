0ccbad96dc053fb9dd22ee3cff5da2ca69754754
Pietro Gagliardi <pietro10@mac.com>
2017-01-08 17:37:38 -0500

    Initial Commit

---
+++ ctlineboundstest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ctlineboundstest
+//
+//  Created by Pietro Gagliardi on 1/8/17.
+//  Copyright © 2017 Pietro Gagliardi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ ctlineboundstest/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  ctlineboundstest
+//
+//  Created by Pietro Gagliardi on 1/8/17.
+//  Copyright © 2017 Pietro Gagliardi. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+	// Insert code here to tear down your application
+}
+
+
+@end
